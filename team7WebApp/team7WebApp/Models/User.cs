be06ea6f3e58c5bf7016ea881199bfdd6d4d1a5d
Arianna Larson <00939300>
2018-03-24 09:24:40 -0400

    Added Role to User Table

--- team7WebApp/team7WebApp/Models/User.cs
+++ team7WebApp/team7WebApp/Models/User.cs
@@ -13,5 +13,6 @@ namespace team7WebApp.Models
         public int DepartmentId { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
+        public int RoleId { get; set; }
     }
 }
