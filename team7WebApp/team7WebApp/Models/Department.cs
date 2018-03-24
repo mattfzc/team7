deba9f69092989df560a839ffe453c97af0a3887
Arianna Larson <00939300>
2018-03-24 09:35:50 -0400

    Added Department table

---
+++ team7WebApp/team7WebApp/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team7WebApp.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
