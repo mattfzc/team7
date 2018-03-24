88011ff93ce766a6acb222072ce0c16c11a732c5
Eric Reed <septem216@gmail.com>
2018-03-24 09:03:57 -0400

    created role table

---
+++ team7WebApp/team7WebApp/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace team7WebApp.Models
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        public string RoleName { get; set; } //eg, Inqiuerer, SME
+
+        /*if Requests true and Assign false, Liazon
+          if Requests true and Assign true, Coordinator*/
+        public bool CanTakeRequests { get; set; }//if true, role is Liazon
+        public bool CanAssign { get; set; }//if true, coordinator
+    }
+}
